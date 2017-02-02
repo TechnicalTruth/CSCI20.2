6ca30a3eefaf82b91b02775d8bb8884dab44f683
Juan Lemus <jlemus002@student.butte.edu>
2017-02-02 23:09:58 +0000

    lab12 submission

---
+++ Lab12/lab12.cpp
@@ -0,0 +1,34 @@
+//Juan Lemus
+//Lab Description: Keyboard painting
+//Date: 2/2/17
+
+#include <iostream>
+using namespace std;
+
+int main ()
+
+{
+    cout << "         666666                " << endl;
+    cout << "        666  666               " << endl;
+    cout << "      6666     66666           " << endl;
+    cout << "    66666  [==]--[==]          " << endl;
+    cout << "  666666        xxxxxxxx       " << endl;
+    cout << "   6666666         xxxxxxxxxx  " << endl;
+    cout << "    6666666    6666666         " << endl;
+    cout << "      6666666666666            " << endl;
+    cout << "        666666666              " << endl;
+    cout << "         666666                " << endl;
+    cout << "       6666666666              " << endl;
+    cout << "     66666   66666             " << endl;
+    cout << "   66666       66666           " << endl;
+    cout << " 66666           66666         " << endl;
+    cout << "66666             66666        " << endl;
+    cout << " 66666           66666         " << endl;
+    cout << "   66666       66666           " << endl;
+    cout << "     66666   66666             " << endl;
+    cout << "       666666666               " << endl;
+    cout << "    666666666666666            " << endl;
+    cout << "66666666666666666666666        " << endl;
+    
+    return 0;
+    }
