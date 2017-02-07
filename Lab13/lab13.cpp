64b7160da46417dbd4e1524ba4a4e6ffaf0e4bd1
Juan Lemus <jlemus002@student.butte.edu>
2017-02-07 22:26:17 +0000

    lab1.3 submission

--- Lab13/lab13.cpp
+++ Lab13/lab13.cpp
@@ -13,17 +13,24 @@ int main()
     int yearChosen = 0;
     int popGrowth = 0;
     int currPopulation = 324473878;
+    int bbyBirthrate = 0;
     
     
 
     
-    cout << "Welcome to Population Calculation, meeting your population estimation needs since 2017!" << endl; 
+    cout << "Welcome to Population Calculation, meeting your population estimation needs since 2017!" << endl;
     
-    cout << "Please input how many years ahead would you like to see: " << endl;
+    cout << "Please enter the current U.S. population: "<< endl;
+    cin >> currPopulation;
+    
+    cout << "Please enter the current U.S. growthrate (per minute): ";
+    cin >> bbyBirthrate;
+    
+    cout << "Please input how many years ahead would you like to see?" << endl;
     cin >> yearsAhead;
     
     yearChosen = 2017 + yearsAhead;
-    popGrowth = 2102400 * yearsAhead;                    // 2,102,400 comes from 4(net gain of one person every 15 sec.)*60*24*365
+    popGrowth = bbyBirthrate * 525600 * yearsAhead;                    // 525600 comes from 60*60*24*365 
     estPopulation = currPopulation + popGrowth;
     
     cout << "You chose to estimate the U.S. population " << yearsAhead << " years in the future." << endl;
