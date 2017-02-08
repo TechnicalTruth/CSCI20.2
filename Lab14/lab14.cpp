86fbfd4331a05292a2a2ad5c4a7906e7d0b7c82f
Juan Lemus <jlemus002@student.butte.edu>
2017-02-08 04:00:14 +0000

    lab1.4 submission

--- Lab14/lab14.cpp
+++ Lab14/lab14.cpp
@@ -1,5 +1,6 @@
-//name: Juan Lemus
+//Name: Juan Lemus
 //Lab Description: Coinstar emulator majiggy
+//Real Description: Program that converts the amount of cents inputed into a dollar value minus tax (assuming the most efficient combination of coins is used)
 //Date: 2/4/17
 
 #include <iostream>
@@ -15,24 +16,26 @@ int main(){
     int numNickl = 0;
     int numPenn = 0;
     int numCent= 0;
-    
+        
     double cashBack = 0.0;
     double totalCash = 0.0;
+                                //variables created
     
     cout << "Please deposit money now." << endl;
     cin >> numCent;
     
     cout << "Total money deposited: ";
-    cout << numCent << " cents" << endl;
+    cout << numCent << " cents." << endl;
     
     
     numQuart = numCent / 25;
     numDimes = (numCent % (numQuart * 25)) / 10;
-    numNickl = (numCent % ((numQuart * 25) + (numDimes * 10)) )/ 5;
-    numPenn = (numCent % ((numQuart * 25) + (numDimes * 10) + (numNickl * 5))) / 1;
+    numNickl = (numCent % ((numQuart * 25) + (numDimes * 10)))/ 5;                      //These equations are used to figure the ammount of Quarters, Dimes, Nickels, and Pennies
+    numPenn = (numCent % ((numQuart * 25) + (numDimes * 10) + (numNickl * 5))) / 1;    //that were put into the 'numCent' variable (respectively). 
+    
     totalCash = numCent;
-    totalCash /= 100.0;
-    cashBack = numCent - (numCent * 0.109);
+    totalCash /= 100.0;                                                                 // I set the two variables equal to each other in order to maintain logistical order and readability 
+    cashBack = numCent - (numCent * 0.109);                                             // Yes I realize this is not in any way necessary 
     cashBack /= 100.0;
     
     cout << "Number of quarters: " << numQuart << endl;
@@ -42,7 +45,7 @@ int main(){
     cout << "Your total is $" << totalCash << endl;
     cout << "Now there will be a 10.9% fee applied." << endl;
     cout << "Your cash voucher will have a value of: $" << cashBack << endl;
-    
+                                                                                        // All of the outputs used once user has entered input
     return 0;
     
 }
