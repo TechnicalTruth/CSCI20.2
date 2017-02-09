21b5856762fb6a9b70253247573dff32af6b9195
Juan Lemus <jlemus002@student.butte.edu>
2017-02-09 22:59:05 +0000

    lab1.5 submission

--- Lab15/lab15.cpp
+++ Lab15/lab15.cpp
@@ -3,12 +3,12 @@
 // Date: 2/7/17
 
 #include <iostream>
-#include <string>
+#include <string>                   //including this library is necessary in order to use strings
 
 using std :: cin;
 using std :: cout;
 using std :: endl;
-using std :: string;
+using std :: string;                //this is also requried considering 'using namespace std;' is not being used
 
 int main(){
     
@@ -16,7 +16,7 @@ int main(){
     float numMedals = 0.0;
     string songName = "";
     string location = "";
-    string noun = "";
+    string noun = "";               //variables created
     
     cout << "Please enter a letter: ";
     cin >> letter;
@@ -27,13 +27,14 @@ int main(){
     cout << "Please enter a noun: ";
     cin >> noun;
     cout << "Please enter a location: ";
-    cin >> location;
+    cin >> location;                            //variables set to user's input
     
     cout << "Juan wants to get an '" << letter << "' in his programming and algorithms class.";
     cout << " His professor was flaberghasted at the idea and said it couldn't be done but, ";
     cout << "in the spirit of Tom Brady's 5th ring, his professor is willing to automatically give him his desired grade" << letter;
     cout << "' if he can sing '" << songName << "' verbatum and if he can guess if the U.S. will earn more or less than " << numMedals << " medals at the next " << noun << " games in ";
     cout << location << "." << endl;
+                                                //user input is placed into a predetermined arrangement of words
     
     /*Juan wants to get an ___ in his programming and algorithms class. In the spirit of comradery, his professor is willing 
     to automatically give him that grade if he can sing ____ verbatum and if he can guess if U.S. will earn more or less than ___ medals at the next 
