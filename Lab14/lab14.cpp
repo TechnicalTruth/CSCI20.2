39ca96278b19fd67ea0d7d1c33393c8decbdb78e
Juan Lemus <jlemus002@student.butte.edu>
2017-02-17 00:25:10 +0000

    lab1.4 submission

--- Lab14/lab14.cpp
+++ Lab14/lab14.cpp
@@ -27,24 +27,34 @@ int main(){
     cout << "Total money deposited: ";
     cout << numCent << " cents." << endl;
     
+    /*These equations are used to figure the amount of Quarters, Dimes, Nickels,
+    and Pennies that were put into the 'numCent' variable (respectively). */
     
     numQuart = numCent / 25;
     numDimes = (numCent % (numQuart * 25)) / 10;
-    numNickl = (numCent % ((numQuart * 25) + (numDimes * 10)))/ 5;                      //These equations are used to figure the amount of Quarters, Dimes, Nickels, and Pennies
-    numPenn = (numCent % ((numQuart * 25) + (numDimes * 10) + (numNickl * 5))) / 1;    //that were put into the 'numCent' variable (respectively). 
+    numNickl = (numCent % ((numQuart * 25) + (numDimes * 10)))/ 5;                      
+    numPenn = (numCent % ((numQuart * 25) + (numDimes * 10) + (numNickl * 5))) / 1;    
     
-    totalCash = numCent;                                                                // I set the two variables equal to each other in order to maintain logistical order and readability
+    /* I set the two variables equal to each other in order to maintain
+    logistical order and readability*/
+    
+    totalCash = numCent; 
     totalCash /= 100.0;                                                                 
-    cashBack = numCent - (numCent * 0.109);                                             //
-    cashBack /= 100.0;                                                                  // Yes I realize this is also not in any way necessary or efficient...
+    cashBack = numCent - (numCent * 0.109); 
+    cashBack /= 100.0;  
+    // Yes I realize this is also not in any way necessary or efficient...
     
+    
+    /* Outputs are to present all denominations individually and then together 
+    as a total dollar value
+    All of the outputs used once user has entered input*/    return 0;
     cout << "Number of quarters: " << numQuart << endl;
     cout << "Number of dimes: " << numDimes << endl;
     cout << "Number of nickels: " << numNickl << endl;
     cout << "Number of pennies: " << numPenn << endl;
     cout << "Your total is $" << totalCash << endl;
     cout << "Now there will be a 10.9% fee applied." << endl;
-    cout << "Your cash voucher will have a value of: $" << cashBack << endl;            // Outputs are to present all denominations individually and then together as a total dollar value
-                                                                                        // All of the outputs used once user has entered input    return 0;
+    cout << "Your cash voucher will have a value of: $" << cashBack << endl;
     
+    return 0;
 }
