202bd6dca1382f50c837ef0231d42f15bb607809
Juan Lemus <jlemus002@student.butte.edu>
2017-04-14 04:29:29 +0000

    lab4.2 submission

---
+++ Lab42/lab42.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using std :: cin;
+using std :: cout;
+using std :: endl;
+
+class ARRAYTOOLS{
+    
+    
+    void print(){
+        cout << "Min: " << ******* << endl;
+        cout << "Max: " << ******* << endl;
+        cout << "Your search: " << *** << endl;
+        
+        
+    }
+}
+int main(){
+    
+}
